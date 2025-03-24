a4e488a4d1fc100ce49007b8143f678342c0bb16
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-24 12:05:16 +0530

    UC-5 complete User Input name display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -10,6 +11,7 @@ export class AppComponent {
   title = 'Hello from BridgeLabz';
   imgUrl = 'assets/BL_Logo.jpg';
   url = "https://www.bridgelabz.com";
+  username = "";
 
   launchUrl() {
     window.open(this.url, "_blank");

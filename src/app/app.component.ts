f1fc013184956c440c7cfa4aee6a61b21f0c13ee
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-24 12:06:27 +0530

    UC-6 complete incorrect msg on wrong name format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,16 @@ export class AppComponent {
   imgUrl = 'assets/BL_Logo.jpg';
   url = "https://www.bridgelabz.com";
   username = "";
+  nameError = "";
+
+  checkUserName(){
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.username)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
 
   launchUrl() {
     window.open(this.url, "_blank");

9bdf45f6f16f48785fa2416f307c0c82b5dc8c4f
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-24 12:04:16 +0530

    UC-4 complete OnClick event done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello from BridgeLabz';
   imgUrl = 'assets/BL_Logo.jpg';
+  url = "https://www.bridgelabz.com";
+
+  launchUrl() {
+    window.open(this.url, "_blank");
+  }
 }
